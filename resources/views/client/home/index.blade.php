7a87f9dcf1b84ef68b46afe3a20cd2e041e00e67
Andhi <agungandhita@gmail.com>
2024-03-07 11:37:21 +0700

    user

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -6,7 +6,6 @@
     <div class="text-center mt-2">
         <h1 class="font-bold text-md md:text-3xl text-black">Cari Sesuai Keinginan Anda</h1>
         <h5 class="text-xs md:text-xl font-serif text-black">disini kami menyediakan berbagai macam kategori seperti di bawah ini</h5>
-    </div>
 
 
     <div class="grid grid-cols-2 md:grid md:grid-cols-4 w-full gap-x-5 pt-4 md:pl-8 justify-center items-center">
@@ -59,4 +58,7 @@
             </div>
         </a>
     </div>
+
+</div>
+  
 @endsection
