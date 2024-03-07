6150161e8509245b1b94c1036b9f615d0c6a520e
Andhi <agungandhita@gmail.com>
2024-03-07 16:46:22 +0700

    user

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -61,10 +61,60 @@
         </div>
 
         <div class="mt-2 md:mt-44">
-            <P class="text-black font-semibold text-3xl">
+            <P class="text-black font-semibold text-xl md:text-3xl">
                 Rekomendasi untuk anda
             </P>
         </div>
+
+        <div class="grid grid-cols-3 md:flex justify-between  gap-x-6 container mb-4 mt-3">
+            <button class="w-35 md:w-60 hover:bg-blue-400 hover:border rounded-lg my-4">
+                <p class="text-xs md:text-xl px-2 py-2 font-semibold shadow-best rounded-lg bg-blend-overlay hover hover:text-white">
+                    Laki-Laki
+                </p>
+            </button>
+            <button class="w-35 md:w-60 hover:bg-blue-400 hover:border rounded-lg my-4">
+                <p class="text-md md:text-xl px-2 py-2 font-semibold shadow-best rounded-lg bg-blend-overlay hover hover:text-white">
+                    Laki-Laki
+                </p>
+            </button>
+            <button class="w-35 md:w-60 hover:bg-blue-400 hover:border rounded-lg my-4">
+                <p class="text-md md:text-xl px-2 py-2 font-semibold shadow-best rounded-lg bg-blend-overlay hover hover:text-white">
+                    Laki-Laki
+                </p>
+            </button>
+            <button class="w-35 md:w-60 hover:bg-blue-400 hover:border rounded-lg my-4">
+                <p class="text-md md:text-xl px-2 py-2 font-semibold shadow-best rounded-lg bg-blend-overlay hover hover:text-white">
+                    Laki-Laki
+                </p>
+            </button>
+            <button class="w-35 md:w-60 hover:bg-blue-400 hover:border rounded-lg my-4">
+                <p class="text-md md:text-xl px-2 py-2 font-semibold shadow-best rounded-lg bg-blend-overlay hover hover:text-white">
+                    Laki-Laki
+                </p>
+            </button>
+
+            <button class="w-35 md:w-60 hover:bg-blue-400 hover:border rounded-lg my-4">
+                <p class="text-md md:text-xl px-2 py-2 font-semibold shadow-best rounded-lg bg-blend-overlay hover hover:text-white">
+                    Laki-Laki
+                </p>
+            </button>
+        </div>
+
+        <div class="container">
+
+            <div class="card card-compact w-96 bg-base-100 shadow-xl">
+                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 
     </div>
 @endsection
