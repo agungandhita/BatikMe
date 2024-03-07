210b5fc75593d0f69ec0a4a69158ec33d44bd6e2
Andhi <agungandhita@gmail.com>
2024-03-07 21:09:06 +0700

    user

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -112,33 +112,63 @@
             </button>
         </div>
 
-        <div class="px-8 grid grid-cols-2 md:grid-cols-4 gap-x-3 border mb-40">
+        <div class="px-8 grid grid-cols-2 md:grid-cols-4 gap-x-3  mb-20">
             
             <div class="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
-                <div class="h-44 rounded-lg border border-red-600" >
-                    <img src="{{ asset ('img/dd.jpeg') }}" class="">
+                <div class="h-44 rounded-lg" >
+                    <img src="{{ asset ('img/dd.jpeg') }}" class="rounded-lg">
                 </div>
             
                 <div class="w-30 -mt-10 md:mt-24 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
-                    <h3 class="py-2 font-bold text-sm tracking-wide text-center text-gray-800 uppercase dark:text-white">Nike Revolt</h3>
+                    <h3 class="py-2 font-semibold text-sm tracking-wide text-center text-gray-800 uppercase dark:text-white">Nike Revolt</h3>
             
                     <div class="flex items-center justify-between gap-x-5 px-3 py-2 bg-gray-200">
-                        <span class="text-xs font-bold text-black">$129</span>
+                        <span class="text-sm font-bold text-black">$129</span>
                         <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add to cart</button>
                     </div>
                 </div>
             </div>
 
             <div class="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
-                <div class="h-44 bg-gray-300 bg-center bg-cover rounded-lg" >
-                    <img src="{{ asset ('img/dd.jpeg') }}" alt="">
+                <div class="h-44 rounded-lg" >
+                    <img src="{{ asset ('img/dd.jpeg') }}" class="rounded-lg">
                 </div>
             
-                <div class="w-30 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
-                    <h3 class="py-2 font-bold text-sm tracking-wide text-center text-gray-800 uppercase dark:text-white">Nike Revolt</h3>
+                <div class="w-30 -mt-10 md:mt-24 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
+                    <h3 class="py-2 font-semibold text-sm tracking-wide text-center text-gray-800 uppercase dark:text-white">Nike Revolt</h3>
             
                     <div class="flex items-center justify-between gap-x-5 px-3 py-2 bg-gray-200">
-                        <span class="text-xs font-bold text-black">$129</span>
+                        <span class="text-sm font-bold text-black">$129</span>
+                        <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add to cart</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
+                <div class="h-44 rounded-lg" >
+                    <img src="{{ asset ('img/dd.jpeg') }}" class="rounded-lg">
+                </div>
+            
+                <div class="w-30 -mt-10 md:mt-24 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
+                    <h3 class="py-2 font-semibold text-sm tracking-wide text-center text-gray-800 uppercase dark:text-white">Nike Revolt</h3>
+            
+                    <div class="flex items-center justify-between gap-x-5 px-3 py-2 bg-gray-200">
+                        <span class="text-sm font-bold text-black">$129</span>
+                        <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add to cart</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
+                <div class="h-44 rounded-lg" >
+                    <img src="{{ asset ('img/dd.jpeg') }}" class="rounded-lg">
+                </div>
+            
+                <div class="w-30 -mt-10 md:mt-24 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
+                    <h3 class="py-2 font-semibold text-sm tracking-wide text-center text-gray-800 uppercase dark:text-white">Nike Revolt</h3>
+            
+                    <div class="flex items-center justify-between gap-x-5 px-3 py-2 bg-gray-200">
+                        <span class="text-sm font-bold text-black">$129</span>
                         <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add to cart</button>
                     </div>
                 </div>
