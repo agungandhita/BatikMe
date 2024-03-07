7219ee33c33714e2c0c65c7b2558a96e5eb87cb0
Andhi <agungandhita@gmail.com>
2024-03-07 12:31:10 +0700

    user

--- resources/views/client/partials/footer.blade.php
+++ resources/views/client/partials/footer.blade.php
@@ -1,7 +1,7 @@
-<footer class="footer p-10 bg-base-200 text-base-content border-t-2 text-black">
+<footer class="footer p-10 bg-base-200 text-black-content border-t-2">
     <aside>
      <img src="{{ asset ('img/w.png') }}" class="w-40 h-20" alt="">
-      <p class="text-sm md:text-xl">BatikMe.ID.<br>'preserving the art of ikat weaving since 1989'</p>
+      <p class="text-sm md:text-xl text-black">BatikMe.ID.<br>'preserving the art of ikat weaving since 1989'</p>
     </aside> 
     <nav>
       <h6 class="footer-title">Services</h6> 
