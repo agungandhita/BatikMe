4bf0bd19690b916fab0fd0af01ea08caf52cf155
Andhi <agungandhita@gmail.com>
2024-03-07 21:14:21 +0700

    user

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -124,7 +124,7 @@
             
                     <div class="flex items-center justify-between gap-x-5 px-3 py-2 bg-gray-200">
                         <span class="text-sm font-bold text-black">$129</span>
-                        <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add to cart</button>
+                        <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">lihat detail</button>
                     </div>
                 </div>
             </div>
@@ -139,7 +139,7 @@
             
                     <div class="flex items-center justify-between gap-x-5 px-3 py-2 bg-gray-200">
                         <span class="text-sm font-bold text-black">$129</span>
-                        <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add to cart</button>
+                        <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">lihat detail</button>
                     </div>
                 </div>
             </div>
@@ -154,7 +154,7 @@
             
                     <div class="flex items-center justify-between gap-x-5 px-3 py-2 bg-gray-200">
                         <span class="text-sm font-bold text-black">$129</span>
-                        <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add to cart</button>
+                        <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">lihat detail</button>
                     </div>
                 </div>
             </div>
@@ -169,7 +169,7 @@
             
                     <div class="flex items-center justify-between gap-x-5 px-3 py-2 bg-gray-200">
                         <span class="text-sm font-bold text-black">$129</span>
-                        <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Add to cart</button>
+                        <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">lihat detail</button>
                     </div>
                 </div>
             </div>
