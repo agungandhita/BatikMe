10442bf1375b78f444a592566d7a9c01b1637608
Andhi <agungandhita@gmail.com>
2024-03-08 11:35:39 +0700

    baru

--- resources/views/client/partials/end.blade.php
+++ resources/views/client/partials/end.blade.php
@@ -6,7 +6,6 @@
 <script src="https://unpkg.com/alpinejs" defer=""></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/zxcvbn/4.4.2/zxcvbn.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js" integrity="sha512-fD9DI5bZwQxOi7MhYWnnNPlvXdp/2Pj3XSTRrFs5FQa4mizyGLnJcN6tuvUS6LbmgN1ut+XGSABKvjN0H6Aoow==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="{{ asset('TW-ELEMENTS-PATH/dist/js/index.min.js') }}"></script>
 
 
 
