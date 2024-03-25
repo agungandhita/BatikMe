720e2b442c920132d4c0873b9760eee6094e788b
Andhi <agungandhita@gmail.com>
2024-03-25 21:58:46 +0700

    baru

--- resources/views/client/partials/end.blade.php
+++ resources/views/client/partials/end.blade.php
@@ -6,6 +6,7 @@
 <script src="https://unpkg.com/alpinejs" defer=""></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/zxcvbn/4.4.2/zxcvbn.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js" integrity="sha512-fD9DI5bZwQxOi7MhYWnnNPlvXdp/2Pj3XSTRrFs5FQa4mizyGLnJcN6tuvUS6LbmgN1ut+XGSABKvjN0H6Aoow==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/alpinejs@2.x.x/dist/alpine.min.js" defer></script>
 
 
 
--- resources/views/client/produk/_spek.blade.php
+++ resources/views/client/produk/_spek.blade.php
@@ -32,7 +32,7 @@
             </h1>
         </div>
     </div>
-    
+
 
 
 </div>
@@ -44,7 +44,11 @@
     </h1>
 
     <p class="text-sm font-serif text-black pt-2 md:text-lg">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus repudiandae at sint eligendi consequuntur non nisi vitae, doloribus aliquid voluptatibus, nulla dolore quia repellat quas velit maxime beatae quidem molestiae.Illo tempora hic, quos veniam dolorem culpa molestias deleniti autem facilis. Voluptatem vel maiores quas? Excepturi fuga corrupti impedit earum iste recusandae accusamus dolor beatae labore, asperiores cumque ipsum exercitationem.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus repudiandae at sint eligendi consequuntur non
+        nisi vitae, doloribus aliquid voluptatibus, nulla dolore quia repellat quas velit maxime beatae quidem
+        molestiae.Illo tempora hic, quos veniam dolorem culpa molestias deleniti autem facilis. Voluptatem vel maiores
+        quas? Excepturi fuga corrupti impedit earum iste recusandae accusamus dolor beatae labore, asperiores cumque
+        ipsum exercitationem.
     </p>
 </div>
 
@@ -61,47 +65,29 @@
             </svg>
             tambahkan keranjang
         </button>
-        <button data-modal-target="crud-modal" data-modal-toggle="crud-modal" 
-            class=" border border-blue-500 bg-white rounded-xl text-blue-600 font-semibold capitalize text-sm hover:rounded-xl w-20 text-center" >
-            Beli
-        </button>
+
+        <div x-data="{ isOpen: false }">
+            <button @click="isOpen = true"
+                class=" border border-blue-500 bg-white rounded-xl text-blue-600 font-semibold capitalize text-sm hover:rounded-xl w-20 text-center">
+                Beli
+            </button>
+
+            <div x-show="isOpen" @click.away="isOpen = false"
+                class="fixed bottom-0 left-0 right-0 bg-gray-800 bg-opacity-75 flex justify-center items-end ">
+                <!-- Konten modal -->
+                <div class="bg-white rounded-t-lg shadow-lg p-4 w-full max-w-md">
+                    <h2 class="text-lg font-bold mb-4">Judul Modal</h2>
+                    <p>Isi modal di sini...</p>
+                    <!-- Tombol untuk menutup modal -->
+                    <button @click="isOpen = false" class="mt-4 bg-red-500 text-white font-bold py-2 px-4 rounded">
+                        Tutup Modal
+                    </button>
+                </div>
+            </div>
+        </div>
 
     </div>
 
 </div>
 
 {{-- modal --}}
-
-<div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-md max-h-full">
-        <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <!-- Modal header -->
-            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                    Create New Product
-                </h3>
-                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal">
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                    </svg>
-                    <span class="sr-only">Close modal</span>
-                </button>
-            </div>
-            <!-- Modal body -->
-            <form class="p-4 md:p-5">
-                <div class="grid gap-4 mb-4 grid-cols-2">
-                    <div class="col-span-2">
-                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                        <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="">
-                    </div>
-                 
-                </div>
-                <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
-                    Add new product
-                </button>
-            </form>
-        </div>
-    </div>
-</div> 
