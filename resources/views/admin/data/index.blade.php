d358568d8545e6ec9f57893c1f1e5550d328c66e
Andhi <agungandhita@gmail.com>
2024-06-08 11:52:47 +0700

    terbaru

--- resources/views/admin/data/index.blade.php
+++ resources/views/admin/data/index.blade.php
@@ -61,7 +61,7 @@
                                 </a>
                             </div>
                         </div>
-                        <div class="flex items-center ml-auto space-x-2 sm:space-x-3">
+                        {{-- <div class="flex items-center ml-auto space-x-2 sm:space-x-3">
 
                             <a href="#"
                                 class="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
@@ -73,7 +73,7 @@
                                 </svg>
                                 Export
                             </a>
-                        </div>
+                        </div> --}}
                     </div>
                 </div>
             </div>
